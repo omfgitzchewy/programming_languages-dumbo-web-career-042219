92bbdaaa66f52f9261c2a5b6f684c1448d93c9b0
Aaron Chu <aaronchu357@gmail.com>
2019-04-09 02:57:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,11 @@
 def reformat_languages(languages)
   new_hash = {}
+  arr = []
   languages.each do |style, language|
     language.each do |type, value|
-      new_hash << {language => {type => value, :style => [style]}}
+      arr << style
+      new_hash[language] = {[type] = value, [style] = arr[]}
+
     end
   end
   new_hash
