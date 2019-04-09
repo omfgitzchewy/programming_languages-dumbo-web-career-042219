88e8e5e89a01e1ef4ebb5bfcd5f9824235af1f30
Aaron Chu <aaronchu357@gmail.com>
2019-04-09 03:15:47 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,8 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
-    language.each do |type, value|
-      
+    language.each do |name, type|
+      new_hash[language] = type
     end
   end
   new_hash
